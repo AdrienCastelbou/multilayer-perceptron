a270c505c5cc6190388ee46ca4a1d2a31a4b4850
Adrien Castelbou <acastelbou1@gmail.com>
2023-01-18 16:15:52 +0100

    start preprocessing

---
+++ main.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import sys
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+features_to_exclude = []#"f3", "f5", "f9", "f10", "f12", "f15", "f18", "f19", "f20", "f24"]
+def load_dataset():
+    if len(sys.argv) != 2:
+        raise Exception("Error : Wrong number of arguments -> Usage : python3 describe.py path/to/dataset.csv")
+    df = pd.read_csv(sys.argv[1])
+    return df
+
+def hist(df):
+    M_df = df[df["State"] == "M"]
+    B_df = df[df["State"] == "B"]
+    for feature in df.columns:
+        if feature != "State":
+            plt.title(feature)
+            plt.hist(M_df[feature].to_numpy(), bins=20, label="M", alpha=0.5)
+            plt.hist(B_df[feature].to_numpy(), bins=20, label="B", alpha=0.5)
+            plt.legend()
+            plt.show()
+
+def show_pair_plot(df):
+    print([feature not in features_to_exclude for feature in df.columns])
+    df = df[[feature for feature in df.columns if feature not in features_to_exclude]]
+    print(df.columns)
+    sns.pairplot(df,  diag_kind="hist", hue="State", markers='.', height=2)
+    plt.show()
+
+def main():
+    df = load_dataset()
+    df.drop(df.columns[0], axis=1, inplace=True)
+    col_names = ["State"]
+    for i in range(1, 31):
+        col_names.append(f"f{i}")
+    df.columns = col_names
+    #show_pair_plot(df)
+    hist(df)
+    
+
+if __name__ == "__main__":
+    main()
