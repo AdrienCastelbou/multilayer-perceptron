a817792dc58511815deafbac137ff6b487420af0
Adrien CASTELBOU <acastelb@e3r11p16.clusters.42paris.fr>
2023-01-25 17:44:53 +0100

    clean

--- Layer.py
+++ Layer.py
@@ -1,47 +1,44 @@
 import numpy as np
 
-class Layer():
+class Layer:
     def __init__(self):
         pass
-
+    
     def forward(self, input):
         return input
 
     def backward(self, input, grad_output):
-        num_units = input.shape[1]
-        d_layer_d_inputs = np.eye(num_units)
-        return grad_output @ d_layer_d_inputs
-
-
-
+        num_units = input.shape[1]   
+        d_layer_d_input = np.eye(num_units)
+        return np.dot(grad_output, d_layer_d_input)
+    
 class ReLU(Layer):
     def __init__(self):
         pass
-
+    
     def forward(self, input):
         relu_forward = np.maximum(0,input)
         return relu_forward
     
     def backward(self, input, grad_output):
         relu_grad = input > 0
-        return grad_output*relu_grad
+        return grad_output*relu_grad 
     
 
 class Dense(Layer):
-    def __init__(self, input_units, output_units, learning_rate=0.1, status="hidden"):
+    def __init__(self, input_units, output_units, learning_rate=0.1):
         self.learning_rate = learning_rate
-        self.weights = np.random.normal(loc=0.0, scale=np.sqrt(2/(input_units + output_units)), size=(input_units, output_units))
+        self.weights = np.random.normal(loc=0.0, 
+                                        scale = np.sqrt(2/(input_units+output_units)), 
+                                        size = (input_units,output_units))
         self.biases = np.zeros(output_units)
-        self.status=status
+        
+    def forward(self,input):
+        return np.dot(input,self.weights) + self.biases
     
-
-    def forward(self, input):
-        return input @ self.weights + self.biases
-    
-
-    def backward(self, input, grad_output):
-        grad_input = grad_output @ self.weights.T
-        grad_weights = input.T @ grad_output
+    def backward(self,input,grad_output):
+        grad_input = np.dot(grad_output, self.weights.T)
+        grad_weights = np.dot(input.T, grad_output)
         grad_biases = grad_output.mean(axis=0)*input.shape[0]
         self.weights = self.weights - self.learning_rate * grad_weights
         self.biases = self.biases - self.learning_rate * grad_biases
